9818e62851f638d7769b7b07d35f303f938dfdb0
Rijkihdy <rizkyhardianti77@gmail.com>
2024-12-04 09:44:40 +0700

    tugas besar siperpus

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'mahasiswa',
+            'email' => 'mahasiswa@unsur.ac.id',
+        ])->assignRole('mahasiswa')->givePermissionTo('view_book');
+
+        User::factory()->create([
+            'name' => 'pustakawan',
+            'email' => 'pustakawan@unsur.ac.id',
+        ])->assignRole('pustakawan')
+        ->givePermissionTo(['edit_book','edit_user']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/book', [BookController::class, 'index'])->name('book');
+});
+
+Route::group(['middleware' => ['role:pustakawan']], function () {
+    Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
+    Route::get('/book/edit/{id}', [BookController::class, 'edit'])->name('book.edit');
+    Route::post('/book/store', [BookController::class, 'store'])->name('book.store');
+    Route::patch('/book/{id}/update', [BookController::class, 'update'])->name('book.update');
+    Route::delete('/book/{id}/delete', [BookController::class, 'destroy'])->name('book.destroy');
+    Route::get('/book/print', [BookController::class, 'print'])->name('book.print');
+    Route::get('/book/export', [BookController::class, 'export'])->name('book.export');
+});
+
+require __DIR__.'/auth.php';
